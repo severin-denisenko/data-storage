5c7823cdad1a9f2cf5d43a741c666731cb8592a4
severin-denisenko <devchonkavkorobchonke@gmail.com>
2022-11-16 11:46:55 +0300

    Add Better Serialization

---
+++ src/Serialize.h
@@ -0,0 +1,53 @@
+//
+// Created by Severin on 16.11.2022.
+//
+
+#ifndef DATA_STORAGE_SERIALIZE_H
+#define DATA_STORAGE_SERIALIZE_H
+
+#include <string>
+#include <alpaca/alpaca.h>
+
+#include "Logger.h"
+
+namespace Serialize{
+    /*
+     * Container for Serializing / Deserializing any data
+     */
+    template <typename T>
+    struct Data{
+        T data;
+    };
+
+    /*
+     * Takes any data with type T and returns std::string
+     */
+    template <typename T>
+    std::string Serialize(const T& data){
+        Data<T> d{data};
+
+        std::vector<uint8_t> bytes;
+        alpaca::serialize(d, bytes);
+
+        std::string str(bytes.begin(), bytes.end());
+        return str;
+    }
+
+    /*
+     * Takes serialized string and return object
+     */
+    template <typename T>
+    T Deserialize(const std::string& str){
+        std::vector<uint8_t> bytes(str.begin(), str.end());
+
+        std::error_code ec;
+        auto data = alpaca::deserialize<Data<T>>(bytes, ec);
+        if (ec) {
+            LOG(WARNING) << ec << std::endl;
+        }
+
+        return data.data;
+    }
+}
+
+#endif //DATA_STORAGE_SERIALIZE_H
--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,7 @@
 
 #include "DataBase.h"
 #include "Logger.h"
-
-#include <alpaca/alpaca.h>
-
-struct Config {
-    std::string device;
-    std::pair<unsigned, unsigned> resolution;
-    std::array<double, 9> K_matrix;
-    std::vector<float> distortion_coeffients;
-    std::map<std::string, std::variant<uint16_t, std::string, bool>> parameters;
-};
+#include "Serialize.h"
 
 int main()
 {
@@ -20,34 +11,14 @@ int main()
 
     DataBase data_base = DataBase("Databases/testdb");
 
-    // Construct the object
-    Config c{"/dev/video0", {640, 480},
-             {223.28249888247538, 0.0, 152.30570853111396,
-              0.0, 223.8756535707556, 124.5606000035353,
-              0.0, 0.0, 1.0},
-             {-0.44158343539568284, 0.23861463831967872, 0.0016338407443826572,
-              0.0034950038632981604, -0.05239245892096022},
-             {{"start_server", bool{true}},
-              {"max_depth", uint16_t{5}},
-              {"model_path", std::string{"foo/bar.pt"}}}};
+    std::vector<double> data = {1, 3, 4, 5};
 
-    // Serialize
-    std::vector<uint8_t> bytes;
-    alpaca::serialize(c, bytes);
-    std::string shit(bytes.begin(), bytes.end());
+    data_base.Put("some shit", Serialize::Serialize(data));
 
-    // Put in DB
-    data_base.Put("some shit", shit);
+    auto new_data = Serialize::Deserialize<std::vector<double>>(data_base.Get("some shit"));
 
-    // Get from DB
-    std::string new_shit = data_base.Get("some shit");
-
-    // Deserialize
-    std::vector<uint8_t> new_bytes(new_shit.begin(), new_shit.end());
-    std::error_code ec;
-    auto object = alpaca::deserialize<Config>(bytes, ec);
-    if (ec) {
-        LOG(WARNING) << ec << std::endl;
+    for(auto item: new_data){
+        LOG(INFO) << item << std::endl;
     }
 
     data_base.Iterate([](const std::string& str){
